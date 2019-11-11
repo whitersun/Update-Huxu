b116b3712cd665c694900fdc395ca4863e35dad8
whitersun <37898596+whitersun@users.noreply.github.com>
2019-11-11 20:18:30 +0800

    Update home-newOne.js

---
+++ js/home-newOne.js
@@ -0,0 +1,153 @@
+const Huxu_json = {
+    'carousel-list': [{
+            headName: '自然農法的魅力，找尋回美麗的生態',
+            description: '亮麗的生態環境，期待你的蒞臨!',
+            name: '鬍鬚青蛙',
+            link: '/master.html',
+            images: '../../images/LR Fix Model/images-14.jpg'
+        },
+        {
+            headName: '在迷人的生態中，漫步欣賞蝴蝶曼妙身影',
+            description: '亮麗的生態環境，期待你的蒞臨!',
+            name: '鬍鬚小蝴蝶',
+            link: '/master.html',
+            images: '../../images/LR Fix Model/images-4.jpg'
+        }
+    ],
+
+    'gallery-grid': [
+        //{
+        //     id: 1,
+        //     name: '香蕉',
+        //     galleryName: 'banna',
+        //     description: '為高熱量水果，熱帶地區廣泛栽培食用。香蕉味香、富於營養，終年可收穫，它富含大量醣類物質及它營養成分，在某些熱帶地區可作為主要糧食。',
+        //     images: '../../images/60.jpg',
+        //     link: '#banana'
+        // },
+        {
+            id: 2,
+            name: '四季豆',
+            galleryName: 'green-bean',
+            description: '生育旺盛、蔓性，白花，綠莢，莢型端直粗圓，無筋，肉厚，莢質細嫩，不易老化。適合用來炒食、川燙',
+            images: '../../images/61.jpg',
+            link: '#bean'
+        },
+        {
+            id: 3,
+            name: '麻竹筍',
+            galleryName: 'bamboo',
+            description: '外型筆直、呈圓錐狀，筍殼略帶淡綠黃色，肉質較粗、纖維多，適合切片煮湯、切絲快炒、醃製筍醬，或曬成筍乾做成其他料理。',
+            images: '../../images/64.jpg',
+            link: 'bamboo'
+        },
+        {
+            id: 4,
+            name: '水果玉米',
+            galleryName: 'corn',
+            description: '特點為皮薄無渣、口感細膩、質脆而甜，所含水分較高，生吃有解暑的功效。',
+            images: '../../images/62.jpg',
+            link: '#popcrop'
+        },
+        {
+            id: 5,
+            name: '小番茄',
+            galleryName: 'tomato',
+            description: '番茄富含紅素和多種營養，是對身體很好的蔬果。外型小巧可愛，口感圓潤，可當生菜沙拉、夾土司食用，或再拌炒肉類、海鮮時加入對切的小茄，增加酸甜風味和營養。',
+            images: '../../images/60.jpg',
+            link: '#littleTomato'
+        },
+        {
+            id: 6,
+            name: '金鑽鳳梨',
+            galleryName: 'pinapple',
+            description: '此植株特性為生育強、果實大、肉質軟、多汁、纖維稍粗、鮮食加工皆宜。果實小、肉黃纖維細、果心小、味甜香濃。',
+            images: '../../images/65.jpg',
+            link: '#pinapple'
+        }
+    ],
+
+    'new-list': [{
+            id: 1,
+            title: '藏在芒果園的【山豬窩】',
+            description: '山豬一點都不笨！如同其它脊椎動物，懂得利用工具，善加就地取材，採用堆疊鑲嵌的豬豬工法，造了一個堅不可摧的豬窩，還造了一個隱秘的豬豬門，你找得到嗎？',
+            images: 'https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/71943847_511340269458674_8077216763596505088_n.jpg?_nc_cat=106&_nc_oc=AQn-Hw_LENCwgIYaR7cZiVWEiZDq-26idxiiJQoVncx5cLRVZds8QkayTHvr4eLa0DxDrgwYpEvDRaHoAIhk5WR_&_nc_ht=scontent-tpe1-1.xx&oh=1e67a1abe2e26361587d8a877aab2514&oe=5E1BBA0F',
+            link: 'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3D511341012791933%26id%3D255485758377461&width=500'
+        },
+        {
+            id: 2,
+            title: '「植物醫生」',
+            description: '山豬一點都不笨！如同其它脊椎動物，懂得利用工具，善加就地取材，採用堆疊鑲嵌的豬豬工法，造了一個堅不可摧的豬窩，還造了一個隱秘的豬豬門，你找得到嗎？',
+            images: 'https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/71773218_504512026808165_4766315466832478208_o.jpg?_nc_cat=107&_nc_oc=AQkV8XD7aH1ExPNBFn630uI2RpnMtZYXIYsAJ2OvZzlAsMtPhpxP15G4cNqiIqjMybJ70BzhxOcUtXZSBIOSWHKF&_nc_ht=scontent-tpe1-1.xx&oh=36f7d073cb2326c3cf1b41745815e28c&oe=5E362059',
+            link: 'https://www.facebook.com/permalink.php?story_fbid=504515506807817&id=255485758377461'
+        },
+        {
+            id: 3,
+            title: '【跑山雞夏令營】',
+            description: '今年的成員還是只有隊長跟副隊長',
+            images: 'https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/66023965_457673694825332_6113728878901985280_o.jpg?_nc_cat=111&_nc_oc=AQkFX63v3i90XKhfVfMiGOih5smoumIcnb27PIdzOth9NY3-dWDHjSKpRAibaX10MwYsWNybw0inRYOs_bE1DH1g&_nc_ht=scontent-tpe1-1.xx&oh=d0ebb54073b7a5687598300778e97d6e&oe=5E24133C',
+            link: 'https://www.facebook.com/permalink.php?story_fbid=457679268158108&id=255485758377461'
+        }
+    ]
+};
+
+document.querySelector('#gallery-grid-photo').innerHTML = `
+    <div class="d-block pb-2">
+        <img onclick="openFarming(event, 'banana')" id="defaultOpen" src="../../images/59.jpg" class="img-of__cover w-100">
+        <div class="caption-inner">
+            <h5 class="font-bold space-2">香蕉</h5>
+        </div>
+    </div>
+
+    ${Huxu_json['gallery-grid'].map((items) => {
+        
+        return `
+            <div class="d-block pb-2">
+                <img onclick="openFarming(event, '${items.galleryName}')" src=${items.images} class="img-of__cover w-100" />
+                <div class="caption-inner">
+                    <h5 class="font-bold space-2">${items.name}</h5>
+                </div>
+            </div>
+        `;
+    }).join('')}
+`;
+
+document.querySelector('#gallery-grid-text').innerHTML = `
+
+    <div id="banana" class="tabcontent uk-card uk-card-hover uk-card-body">
+        <h5 class="fs-headline_sm font-bold space-2 text-left">香蕉</h5>
+        <p class="fs-22 space-1 lh-15 text-justify">為高熱量水果，熱帶地區廣泛栽培食用。香蕉味香、富於營養，終年可收穫，它富含大量醣類物質及它營養成分，在某些熱帶地區可作為主要糧食。</p> 
+        <a href="#banana" class="uk-button uk-button-secondary fs-22 space-1 mr-auto">詳細資訊</a>
+    </div>     
+
+    ${Huxu_json['gallery-grid'].map((items) => {
+        
+        return `
+            <div id="${items.galleryName}" class="tabcontent uk-card uk-card-hover uk-card-body animated fadeIn">
+                <h5 class="fs-headline_sm font-bold space-2 text-left">${items.name}</h5>
+                <p class="fs-22 space-1 lh-15 text-justify">${items.description}</p> 
+                <a href="${items.link}" class="uk-button uk-button-secondary fs-22 space-1">詳細資訊</a>
+            </div>        `;
+    }).join('')}
+`;
+
+
+// try javascript tabs
+function openFarming(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
+
